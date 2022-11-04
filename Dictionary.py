c439c67d8925e15f768dab689acd4ed241275e6f
Alaaeldinmohamed2020 <117483870+Alaaeldinmohamed2020@users.noreply.github.com>
2022-11-04 22:55:39 +0200

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,9 @@
+# Dictionary
+#employees = {"name" : "Alaa" ,"title": "System Admin" , "age" : 39 ,"courses" :["CCNA","CCNP","MCSE","MCSA 2012","MCSA 2016"]}
+#print(employees)
+#print(employees["name"])
+#print(employees.get("title"))
+#print(employees["courses"][0])
+#employees["company"] = "Egypt Post"
+#employees["title"] = "Network sys admin"
+print(employees)
